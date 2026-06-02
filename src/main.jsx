54f3d3f252af268a71f0681d3ed4ef0388a38140
Nandika Prapanca <nprapanca@gmail.com>
2026-06-02 14:30:21 +0700

    State Management with Context

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,28 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { ThemeContextProvider } from "./context/themeContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+  <ThemeContextProvider>
     <App />
+  </ThemeContextProvider>
   </StrictMode>,
 );
+
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App.jsx";
+// import "./index.css";
+// import Parent from "./latihan/Parent.jsx";
+// import { CounterContextProvider } from "./context/counterContext.jsx";
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <CounterContextProvider>
+//       <Parent />
+//     </CounterContextProvider>
+//   </React.StrictMode>,
+// );
+
