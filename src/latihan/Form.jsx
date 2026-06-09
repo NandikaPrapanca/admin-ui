6b273b7ace9ffa92044aad8f160c6f2196f5858c
Nandika Prapanca <nprapanca@gmail.com>
2026-06-09 13:51:04 +0700

    Post Data

--- src/latihan/Form.jsx
+++ src/latihan/Form.jsx
@@ -1,15 +1,25 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 const Form = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
-    const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(email);
-    console.log(password);
-    console.log("-");
-    };
+    const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  try {
+    const response = await axios.post(
+      "https://jwt-auth-eight-neon.vercel.app/login",
+      { email, password },
+    );
+
+    console.log(response.data);
+    localStorage.setItem("token", response.data.refreshToken);
+  } catch (error) {
+    console.error(error);
+  }
+};
 
   return (
     <div className="p-4">
