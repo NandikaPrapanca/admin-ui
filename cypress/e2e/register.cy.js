9a9b291cd319781432d6314e146e3053945ec6ed
Nandika Prapanca <nprapanca@gmail.com>
2026-06-23 14:11:27 +0700

    Fix Register Testing with Cypress

--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -1,28 +1,36 @@
 describe("User register", () => {
-  it("should display register form", () => {
+  it("should navigate to register page and fill register form", () => {
     cy.viewport(550, 750);
 
-    cy.visit("http://localhost:5173/register");
+    cy.visit("http://localhost:5173/");
+    cy.url().should("include", "/login");
+
+    cy.contains("Create an account").click();
 
     cy.url().should("include", "/register");
 
     cy.get("#name")
       .should("be.visible")
+      .should("have.attr", "placeholder", "Nandika Rizki Prapanca")
       .type("Nandika Rizki Prapanca")
       .should("have.value", "Nandika Rizki Prapanca");
 
     cy.get("#email")
       .should("be.visible")
+      .should("have.attr", "placeholder", "hello@example.com")
       .type("nandika@test.com")
       .should("have.value", "nandika@test.com");
 
     cy.get("#password")
       .should("be.visible")
+      .should("have.attr", "placeholder", "********")
       .type("123456")
       .should("have.value", "123456");
 
     cy.get("button").contains("Sign Up").click();
 
     cy.url().should("include", "/register");
+
+    cy.wait(3000);
   });
 });
